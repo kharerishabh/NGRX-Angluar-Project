73d71de27f42ce92778973783b526406d0ea26de
KHARERISHABH <rishabkhare41@gmail.com>
2024-02-08 21:01:51 +0530

    alternative way of defining actions

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,5 +1,5 @@
-import { createReducer, on } from '@ngrx/store';
-import { increment } from './counter.actions';
+import { Action, createReducer, on } from '@ngrx/store';
+import { CounterActions, INCREMENT, IncrementAction } from './counter.actions';
 
 const initialState = 0;
 // export const counterReducer = createReducer(
@@ -8,9 +8,9 @@ const initialState = 0;
 // );
 
 //Alternative way of creating reducer
-export function counterReducer (state = initialState, action: any){
-    if(action.type === '[Counter] Increment'){
-        return state + action.value
+export function counterReducer (state = initialState, action: CounterActions | Action){
+    if(action.type === INCREMENT){
+        return state + (action as IncrementAction).value
     }
     return state;
 }
